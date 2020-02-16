2c698fc86f4ff5ac17dc1ab64f1f539b1cc171bb
Dima Danyliuk <dimadanylyuk@gmail.com>
2020-02-16 22:29:43 +0200

    Updates

---
+++ precache-manifest.5f830635a69c7e555ef9322d5c318c26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a4a7ae80c2cd3acf507bdc646cd8e7",
+    "url": "/budget_app/index.html"
+  },
+  {
+    "revision": "db5258fa880a4d6bea2a",
+    "url": "/budget_app/static/css/2.a8ef9e24.chunk.css"
+  },
+  {
+    "revision": "8423b7465203a66e00ff",
+    "url": "/budget_app/static/css/main.ae8b4133.chunk.css"
+  },
+  {
+    "revision": "db5258fa880a4d6bea2a",
+    "url": "/budget_app/static/js/2.dd8636ac.chunk.js"
+  },
+  {
+    "revision": "fb41dd5886fb85447735e6cb2809bd6d",
+    "url": "/budget_app/static/js/2.dd8636ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8423b7465203a66e00ff",
+    "url": "/budget_app/static/js/main.8368d9e2.chunk.js"
+  },
+  {
+    "revision": "8a807081c50fd3c645f2",
+    "url": "/budget_app/static/js/runtime-main.b5ea5f7c.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/budget_app/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/budget_app/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/budget_app/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/budget_app/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/budget_app/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/budget_app/static/media/icons-20.ca7934b3.eot"
+  }
+]);
