2ab1eb8bc20f3f6477bedf3d45a12134ec6feeea
Vampirkod <vampirkod@gmail.com>
2020-02-20 23:04:45 +0200

    Update icon, add active state for menu

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
+import { Switch, Route, useHistory, useLocation } from 'react-router-dom';
 import _ from 'lodash';
+import classNames from "classnames";
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
 import HomeIcon from '@material-ui/icons/Home';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import ListIcon from '@material-ui/icons/List';
+import Avatar from "@material-ui/core/Avatar";
+
 import routes from 'router/routes';
 
 
@@ -26,6 +28,22 @@ const useStyles = makeStyles(theme => ({
   },
   avatarBlock: {
     height: 80,
+    paddingTop: 12,
+  },
+  menuIcon: {
+    padding: '4px 0',
+    width: '100%',
+    height: 48,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+    '&:hover': {
+      background: '#efefef',
+    },
+  },
+  menuIconActive: {
+    background: '#dedede',
   },
   topNavigation: {},
   title: {
@@ -46,11 +64,11 @@ const useStyles = makeStyles(theme => ({
 
 const menuItems = [
   {
-    icon: <HomeIcon />,
+    icon: <HomeIcon htmlColor="#3b3b3b" />,
     link: routes.home.path
   },
   {
-    icon: <NotificationsIcon />,
+    icon: <ListIcon htmlColor="#3b3b3b" />,
     link: routes.transactions.path,
   },
 ];
@@ -58,15 +76,25 @@ const menuItems = [
 const BaseLayout: React.FC = ({ children }) => {
   const classes = useStyles();
   const history = useHistory();
+  const location = useLocation();
 
   return (
     <div className={classes.root}>
       <div className={classes.leftNavigation}>
-        <div className={classes.avatarBlock}></div>
+        <div className={classes.avatarBlock}>
+          <Avatar />
+        </div>
         {menuItems.map(i => (
-          <IconButton onClick={() => history.push(i.link)}>
+          <div
+            key={i.link}
+            onClick={() => history.push(i.link)}
+            className={classNames(
+              classes.menuIcon,
+              { [classes.menuIconActive]: location.pathname === i.link}
+            )}
+          >
             {i.icon}
-          </IconButton>
+          </div>
         ))}
       </div>
       <div>
